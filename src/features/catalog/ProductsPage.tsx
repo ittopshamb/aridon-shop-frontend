4db615695a3d9da021541b29085703588a692c06
blurryface17 <curvepiece@gmail.com>
2023-04-17 12:48:18 +0400

    Add pagination

--- src/features/catalog/ProductsPage.tsx
+++ src/features/catalog/ProductsPage.tsx
@@ -1,6 +1,7 @@
 ﻿import React, { useState, useEffect } from "react";
-import { List, ListItem, ListItemText } from "@mui/material";
+import {List, ListItem, ListItemText, Pagination, Stack, styled} from "@mui/material";
 import {useParams} from 'react-router-dom';
+
 type Product = {
     productId: string;
     productName: string;
@@ -13,27 +14,59 @@ type Product = {
 const ProductsPage = () => {
     const { categoryId } = useParams<{ categoryId: string }>();
     const [products, setProducts] = useState<Product[]>([]);
+    const [pageCount, setPageCount] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(0);
+
+    const perPage = 10;
 
     useEffect(() => {
         const fetchProducts = async () => {
-            const response = await fetch(`http://localhost:7079/products/get_by_category?categoryId=${categoryId}`);
+            const response = await fetch(
+                `http://localhost:7079/products/get_by_category?categoryId=${categoryId}`
+            );
             const data = await response.json();
             setProducts(data.products);
+            setPageCount(Math.ceil(data.products.length / perPage));
         };
         fetchProducts();
     }, [categoryId]);
 
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setCurrentPage(value - 1);
+    };
+
+    const offset = currentPage * perPage;
+
+    const currentProducts = products
+        .slice(offset, offset + perPage)
+        .map((product) => (
+            <ListItem key={product.productId}>
+                <img src={product.image} alt={product.productName} style={{ width: 80, height: 80, marginRight: 16 }} />
+                <ListItemText primary={product.productName} secondary={`$${product.price}`} />
+            </ListItem>
+        ));
+
     return (
         <div>
-            <List>
-                {products.map((product) => (
-                    <ListItem key={product.productId}>
-                        <ListItemText primary={product.productName} secondary={`$${product.price}`} />
-                    </ListItem>
-                ))}
-            </List>
+            <List>{currentProducts}</List>
+            <StyledStack spacing={2}>
+                <Pagination
+                    count={pageCount}
+                    variant="outlined"
+                    shape="rounded"
+                    page={currentPage + 1}
+                    onChange={handlePageChange}
+                />
+            </StyledStack>
         </div>
     );
 };
+
+const StyledStack = styled(Stack)({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "16px",
+});
 
 export default ProductsPage;
